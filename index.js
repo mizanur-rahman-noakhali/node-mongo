4dfcddc3f62503bab48e5c6e8abdd4c9efc2e69a
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-12-03 23:45:19 +0600

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 
-
+const cors =require('cors')
 
 
 
 const app = express()
 
+app.use(cors());
 
+
+const users=['aksh','habib','badol','joy','ridoy','asif']
 
 app.get("/",(req,res)=>{
-  res.send("Thank you");
+
+  const fruit={
+    product:"Banana",
+    price:220
+  }
+
+  res.send(fruit);
 })
 
+app.get("/fruit/banana",(req,res)=>{
+
+  res.send({fruit:"banana",quantity:1000,price:100});
+})
+
+app.get("/users/:id",(req,res)=>{
+  const id=req.params.id;
+  console.log(req.query.sort)
+   const name=users[id]
+   res.send({id,name})
+})
 
 app.listen(3000,()=> console.log("listening to port 3000"))
